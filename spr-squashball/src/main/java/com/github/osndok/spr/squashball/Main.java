9260c76b47e50361e2b9b098920cb98776d6297e
Robert Hailey <git@osndok.com>
2021-04-24 09:27:50 -0500

    Starting work on new module: spr-squashball

---
+++ spr-squashball/src/main/java/com/github/osndok/spr/squashball/Main.java
@@ -0,0 +1,13 @@
+package com.github.osndok.spr.squashball;
+
+/**
+ * Input:
+ * (1) A source directory full of files,
+ * (2) An output filename for the final squashball,
+ * (3) A password to encode the table-of-contents
+ */
+public
+class Main
+{
+
+}
