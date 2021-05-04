5446edd5994828e5986b63ecd6ab2edb23d80ba5
Robert Hailey <git@osndok.com>
2021-05-03 22:01:12 -0500

    Spr1Tuple encapsulates the input/encryption/identification logic

---
+++ spr-common/src/main/java/com/github/osndok/spr/Spr1Tuple.java
@@ -0,0 +1,48 @@
+package com.github.osndok.spr;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Intended for small files that easily fit into memory. This class will provide all the spr1 data & hashes
+ * (given cleartext data) without the aid of an underlying repo.
+ */
+public
+class Spr1Tuple
+{
+    public final
+    byte[] clearTextBytes;
+
+    public final
+    byte[] privateHash;
+
+    public final
+    byte[] encryptedBytes;
+
+    public final
+    byte[] publicHash;
+
+    public final
+    Spr1Key spr1Key;
+
+    public
+    Spr1Tuple(File file) throws IOException
+    {
+        clearTextBytes = Files.readAllBytes(file.toPath());
+        privateHash = Sha1Repo.getSha1Sum(clearTextBytes);
+        encryptedBytes = new Spr1Encryption(privateHash).encrypt(clearTextBytes);
+        publicHash = Sha1Repo.getSha1Sum(encryptedBytes);
+        spr1Key = new Spr1Key(publicHash, privateHash);
+    }
+
+    public
+    Spr1Tuple(byte[] bytes) throws IOException
+    {
+        clearTextBytes = bytes;
+        privateHash = Sha1Repo.getSha1Sum(clearTextBytes);
+        encryptedBytes = new Spr1Encryption(privateHash).encrypt(clearTextBytes);
+        publicHash = Sha1Repo.getSha1Sum(encryptedBytes);
+        spr1Key = new Spr1Key(publicHash, privateHash);
+    }
+}
